e1eaa012566d9450295d15846390e7aa524f032e
Troy Williams <troyDonwilliams@gmail.com>
2018-03-26 13:44:25 -0500

    I think ram is good

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -2,28 +2,39 @@
  * RAM access
  */
 class RAM {
-    constructor(size) {
-        this.mem = new Array(size);
-        this.mem.fill(0);
-    }
+  constructor(size) {
+    this.mem = new Array(size);
+    this.mem.fill(0);
+  }
 
-    /**
-     * Write (store) MDR value at address MAR
-     */
-    write(MAR, MDR) {
-        // !!! IMPLEMENT ME
-        // write the value in the MDR to the address MAR
-    }
+  /**
+   * Write (store) MDR value at address MAR
+   */
+  write(MAR, MDR) {
+    // !!! IMPLEMENT ME
+    // write the value in the MDR to the address MAR
+    return this.access(MAR, MDR, true);
+  }
 
-    /**
-     * Read (load) MDR value from address MAR
-     * 
-     * @returns MDR
-     */
-    read(MAR) {
-        // !!! IMPLEMENT ME
-        // Read the value in address MAR and return it
+  /**
+   * Read (load) MDR value from address MAR
+   *
+   * @returns MDR
+   */
+  read(MAR) {
+    // !!! IMPLEMENT ME
+    // Read the value in address MAR and return it
+    return this.access(MAR, null, false);
+  }
+
+  access(MAR, MDR, write) {
+    if (write) {
+      this.mem[MAR] = MDR;
+    } else {
+      MDR = this.mem[MAR];
     }
+    return MDR;
+  }
 }
 
 module.exports = RAM;
